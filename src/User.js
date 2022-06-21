4d4595a57459d0678c13cce777716244d1bbae8c
RAHUL GUPTA <42072042+rahul8497@users.noreply.github.com>
2022-06-21 21:16:17 +0530

    Add files via upload

---
+++ src/User.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const User = (props) => {
+  return (
+    <>
+      <h1>Prop Data : {props.rollprops}</h1>
+    </>
+  );
+};
+
+export default User;
---
+++ src/Welcome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Welcome = (props) => {
+  return <>
+  <h1>Hey ! {props.name}</h1>
+  <h2>Welcome to Newton School.</h2>
+  </>
+}
+
+export default Welcome;
